9e4202d84477ee78ad460895132208eb6df116f2
unknown <charifabdeslem@gmail.com>
2018-05-09 15:14:45 +0100

    Update

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 const express = require('express');
+
+var app = express();
+const port = process.env.PPORT || 3000;
+app.use(express.static("public_html"))
+app.set("views","./src/view");
+app.set("view engine","ejs");
+
+app.get("/",(req,res)=>{
+  res.render("index",{username:'JSX username'});
+});
+
+app.listen(3000,(err)=>{
+  console.log("App up and running on port: " + port);
+});
