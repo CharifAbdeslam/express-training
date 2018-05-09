7a91e6a33807cb1abbf5a00fb3487b4a22f6a931
unknown <charifabdeslem@gmail.com>
2018-05-09 16:35:40 +0100

    Update

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
 const express = require('express');
-
-var app = express();
+var menu = "next one";
+const app = express();
+const router = express.Router();
 const port = process.env.PPORT || 3000;
-app.use(express.static("public_html"))
-app.set("views","./src/view");
-app.set("view engine","ejs");
 
-app.get("/",(req,res)=>{
-  res.render("index",{username:'JSX username'});
+
+app.use(express.static("public_html"))
+app.set("views", "./src/view");
+app.set("view engine", "ejs");
+
+router.route("/").get((req, res) => {
+  res.json("Hello welcome to exchange");
+});
+router.route("/list").get((req, res) => {
+  res.json("you are in the list view");
+});
+app.use("/exchange", router);
+app.get("/", (req, res) => {
+  res.render("index", {
+    username: 'JSX username',
+    varmenu: menu
+  });
 });
 
-app.listen(3000,(err)=>{
+app.listen(3000, (err) => {
   console.log("App up and running on port: " + port);
 });
